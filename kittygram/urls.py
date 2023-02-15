3c31bf301b8e1a5275298736fd3f60d2644e4151
Роман Александров <rsaleksandrov@yandex.ru>
2023-02-15 22:08:37 +0300

    D общем коммит

--- kittygram/urls.py
+++ kittygram/urls.py
@@ -1,9 +1,10 @@
-from django.urls import include, path
+from django.urls import path
 
-from cats.views import cat_list
+from cats.views import cat_list, hello, APICatCR, APICatRUD
 
 urlpatterns = [
-   path('cats/', cat_list),
+    path('cats/', cat_list),
+    path('cats2/<int:id>/', APICatRUD.as_view()),
+    path('cats2/', APICatCR.as_view()),
+    path('', hello),
 ]
-
-
